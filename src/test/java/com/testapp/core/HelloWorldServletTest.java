b2fa86ca95977aecd402c8edded774b3dd868cb7
Esteban Andrés Mogollón <estebanmogo@hotmail.com>
2023-12-11 18:26:39 +0000

    Updated HelloWorldServletTest.java

--- src/test/java/com/testapp/core/HelloWorldServletTest.java
+++ src/test/java/com/testapp/core/HelloWorldServletTest.java
@@ -9,34 +9,20 @@ public class HelloWorldServletTest {
 
     private final HttpServletRequest request = mock(HttpServletRequest.class);
     private final HttpServletResponse response = mock(HttpServletResponse.class);
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     private final HelloWorldServlet servlet = new HelloWorldServlet();
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     @BeforeEach
     public void setUp() throws IOException {
-        ServletOutputStream servletOutputStream = new ServletOutputStream() {
-            @Override
-            public void write(int b) {
-                outputStream.write(b);
-            }
-
-            @Override
-            public boolean isReady() {
-            }
-
-            @Override
-            public void setWriteListener(WriteListener writeListener) {
-
-            }
-        };
-
-        when(response.getOutputStream()).thenReturn(servletOutputStream);
+        PrintWriter writer = new PrintWriter(outputStream);
+        when(response.getWriter()).thenReturn(writer);
     }
 
     @Test
     public void doGetWritesHelloWorld() throws ServletException, IOException {
         servlet.doGet(request, response);
+        outputStream.flush(); // Asegúrate de que se escriba todo en el stream
         String result = outputStream.toString();
-        Assertions.assertEquals("Hello, World.", result);
+        Assertions.assertEquals("Hello, World.", result.trim());
     }
 }
