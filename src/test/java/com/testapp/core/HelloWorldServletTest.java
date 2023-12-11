082e6ded8a31c94df8451838fc3cb34a83bbf2f6
Esteban Andrés Mogollón <estebanmogo@hotmail.com>
2023-12-11 18:38:52 +0000

    Updated HelloWorldServletTest.java

--- src/test/java/com/testapp/core/HelloWorldServletTest.java
+++ src/test/java/com/testapp/core/HelloWorldServletTest.java
@@ -1,27 +1,13 @@
 package com.testapp.core;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldServletTest {
 
     @Test
-    public void doGetWritesHelloWorld() throws Exception {
-        // Crear un StringWriter para capturar la salida del servlet
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-
-        // Crear un HelloWorldServlet y simular la respuesta
+    public void getMessage_ReturnsHelloWorld() {
         HelloWorldServlet servlet = new HelloWorldServlet();
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        when(response.getWriter()).thenReturn(writer);
-
-        // Llamar al método doGet y verificar el resultado
-        servlet.doGet(null, response);
-
-        writer.flush();
-        assertEquals("Hello, World.", stringWriter.toString().trim());
+        assertEquals("Hello, World.", servlet.getMessage());
     }
 }
